7a05d763cbdf136045a5a1d92758414baedad9da
marah7 <marahothman744@gmail.com>
2020-12-26 19:15:41 +0100

    commit 3

--- src/container/App.js
+++ src/container/App.js
@@ -6,65 +6,45 @@ import Searchbox from '../componnent/Searchbox';
 import './App.css';
 import ErorrBoundry from '../componnent/ErorrBoundry';
  
+class App extends Component {
 
-class App extends Component
-{
     constructor(){
-        super()
-        this.state={
-            robots:[],
-            searchfield:''
-        }}
-componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users')
-    .then(Response =>Response.json())
-    .then(users=>this.setState({robots:users}));
+        super();
+        this.state = { robots: [], searchfield: ''};
+    }
 
-}
-onSearchChange =(Event)=>{  
-    
-    
-    this.setState({searchfield:Event.target.value})
-    
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response =>Response.json())
+        .then(users=>this.setState({robots:users}));
+    }
 
-
-
-   
-}
+    onSearchChange = (Event) => {  
+       this.setState({ searchfield: Event.target.value });
+    }
         
-    
     render(){
-        const {robots,searchfield}=this.state;
-        const filteredRobots=this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+        const {robots,searchfield} = this.state;
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        });
+        console.log(robots);
         return !robots.length ?
-    
-        
+
                 <div className='tc'>
                     <h1>loading</h1>
                 </div>:
             (
                 <div className='tc'>
                     <h1 className='f1'>robofriends</h1>
-                    
                     <Searchbox searchchange={this.onSearchChange} />
                     <Scroll>
                         <Erorrboundry>
-                        <Cardlist  robots={filteredRobots}/>
+                        <Cardlist robots={filteredRobots}/>
                         </Erorrboundry>
-                    
                     </Scroll>
-                    
                 </div>
-                
             );
-
-        
-        
-
-
     }
-    
 }
 export default App;
